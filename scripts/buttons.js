1bdba82a48fab83b720201ebb00d7e656b53d510
Kai Sano Matsuda <kaisanomatsuda@Kai-Matsuda.local>
2015-06-23 02:16:58 -0400

    added resume to index.html, added script to get docs for it

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -1,11 +1,5 @@
 var main = function() {
-	$('.togglebutton').click(function() {
-		var toggle = $('.toggle');
-		toggle.slideToggle();
-	});
-	
-	$(".scroll").click(function() {
-	});
+
 };
 
 $(document).ready(main);
